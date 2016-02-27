423a565417316fa244e370632d64046315818621
Frank S. Thomas <frank@timepit.eu>
2016-02-27 17:02:00 +0100

    Clean up

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -7,7 +7,7 @@ import org.mdoc.common.model.circe._
 import org.mdoc.common.model.Format.{ Html, Pdf }
 import org.mdoc.common.model.RenderingEngine.LibreOffice
 import play.api.Play.current
-import play.api.libs.json.Json
+import play.api.libs.json.{ Json, JsValue }
 import play.api.libs.ws.{ WS, WSResponse }
 import play.api.mvc.{ Action, AnyContent, Controller, Result }
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -16,42 +16,41 @@ import scodec.bits.ByteVector
 object Api extends Controller {
 
   def templates: Action[AnyContent] = Action.async {
-    val url = current.configuration
-      .getString("adelmo.template-service.url")
-      .fold("")(_ + "/api/template-views")
+    val url = templateServiceUrl + "/api/template-views"
     WS.url(url).get().map(toResult)
   }
 
   def placeholders(name: String): Action[AnyContent] = Action.async {
-    val url = current.configuration
-      .getString("adelmo.template-service.url")
-      .fold("")(_ + s"/api/template-placeholders/$name")
+    val url = templateServiceUrl + "/api/template-placeholders/" + name
     WS.url(url).get().map(toResult)
   }
 
   def renderTemplate(name: String): Action[AnyContent] = Action.async { req =>
-    println(req)
-    println(req.body)
+    val processUrl = templateServiceUrl + "/api/process/" + name
+    val renderUrl = renderingServiceUrl + "/render"
 
-    val tempUrl = current.configuration
-      .getString("adelmo.template-service.url")
-      .fold("")(_ + s"/api/process/$name")
-    val rendUrl = current.configuration
-      .getString("adelmo.rendering-service.url")
-      .fold("")(_ + "/render")
+    val formData = req.body.asFormUrlEncoded.getOrElse(Map.empty)
+    val formDataAsJson = formUrlEncodedToJsonMap(formData)
 
-    val payload2 = req.body.asFormUrlEncoded.getOrElse(Map.empty)
-    val payload = Json.toJson(payload2.mapValues(_.headOption.getOrElse("")))
-    println(payload)
-
-    WS.url(tempUrl).post(payload).flatMap { response =>
-      println(response.body)
-      val input = RenderingInput(JobId("123"), RenderingConfig(Pdf, LibreOffice),
-        Document(Html, ByteVector.view(response.bodyAsBytes)))
-      val json = input.asJson.noSpaces
-      WS.url(rendUrl).post(json).map(toResult)
+    WS.url(processUrl).post(formDataAsJson).flatMap { resp =>
+      val input = RenderingInput(
+        JobId("123"),
+        RenderingConfig(Pdf, LibreOffice), // TODO: output format and renderer should be selectable
+        Document(Html, ByteVector.view(resp.bodyAsBytes))
+      ) // TODO: extract input format from name
+      val inputAsJson = input.asJson.noSpaces
+      WS.url(renderUrl).post(inputAsJson).map(toResult)
     }
   }
+
+  val renderingServiceUrl: String =
+    current.configuration.getString("adelmo.rendering-service.url").getOrElse("")
+
+  val templateServiceUrl: String =
+    current.configuration.getString("adelmo.template-service.url").getOrElse("")
+
+  def formUrlEncodedToJsonMap(map: Map[String, Seq[String]]): JsValue =
+    Json.toJson(map.mapValues(_.mkString(" ")))
 
   def toResult(resp: WSResponse): Result =
     Ok(resp.bodyAsBytes).as(contentTypeOf(resp))
